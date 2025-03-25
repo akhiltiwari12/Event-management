6ae9c5f949cd2c2af29f482eccd1c210743f5712
AKHIL KUMAR TIWARI <159538883+akhiltiwari12@users.noreply.github.com>
2025-03-25 22:06:20 +0530

    11 commit

--- server/Middleware/isAuthenticated.js
+++ server/Middleware/isAuthenticated.js
@@ -3,12 +3,27 @@ import { catchAsyncError } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
-
-export const isAuthenticated=catchAsyncError(async(req,res,next) =>{
-    const token=req.cookies.token;
-    if(!token)
-        return next(new ErrorHandler("User is not Authenticated!",401));
-    const decode=jwt.verify(token,process.env.JWT_SECRET_KEY);
-    req.user=await User.findById(decode.id);
-    next();
+export const isAuthenticated = catchAsyncError(async(req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.replace('Bearer ', '');
+    
+    if (!token) {
+        return next(new ErrorHandler("Authentication token is missing", 401));
+    }
+    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        
+        const user = await User.findById(decoded.id);
+        if (!user) {
+            return next(new ErrorHandler("User not found", 401));
+        }
+        
+        req.user = user;
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            return next(new ErrorHandler("Token has expired", 401));
+        }
+        return next(new ErrorHandler("Invalid token", 401));
+    }
 });
